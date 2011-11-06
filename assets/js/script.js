ad6ab73f28dbca71fd24153d36d3ea7a2ea0670e
atj <tj.abderrahmane@gmail.com>
2011-11-06 20:03:42 +0000

    use data instead of class

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,69 +5,27 @@
 var colors
 $(document).ready(function(){
     colors = {
-		"redone": {
-			main: "#dc3912",
-			side: "#95270c"
-		},
-		"blueone": {
-			main: "#3366cc",
-			side: "#24478f"
-		},
-		"yellowone": {
-			main: "#ff9900",
-			side: "#b36b00"
-		},
-		"pinkone": {
-			main: "#dd4477",
-			side: "#b42152"
-		},
-		"greenone": {
-			main: "#66aa00",
-			side: "#385d00"
-		},
-		"purpleone": {
-			main: "#990099",
-			side: "#4d004c"
-		},
-		"bluetwo": {
-			main: "#316395",
-			side: "#1e3d5b"
-		},
-		"redtwo": {
-			main: "#b82e2e",
-			side: "#7b1f1f"
-		},
-		"greentwo": {
-			main: "#109618",
-			side: "#09510d"
-		},
-		"greenthree":{
-			main: "#6ECF00",
-			side: "#478500"
-		},
-		"bluethree": {
-			main: "#0099c6",
-			side: "#005e7a"
-		},
-		"kamouni": {
-			main: "#6B5E44",
-			side: "#3B3426"
-		},
-		"greyone": {
-			main: "#474747",
-			side: "#212121"
-		},
-		"greytwo": {
-			main: "#333",
-			side: "#0D0D0D"
-		}
+		"redone": { main: "#dc3912", side: "#95270c" },
+		"blueone": { main: "#3366cc", side: "#24478f" },
+		"yellowone": { main: "#ff9900", side: "#b36b00" },
+		"pinkone": { main: "#dd4477", side: "#b42152" },
+		"greenone": { main: "#66aa00", side: "#385d00" },
+		"purpleone": { main: "#990099", side: "#4d004c" },
+		"bluetwo": { main: "#316395", side: "#1e3d5b" },
+		"redtwo": { main: "#b82e2e", side: "#7b1f1f" },
+		"greentwo": { main: "#109618", side: "#09510d" },
+		"greenthree":{ main: "#6ECF00", side: "#478500" },
+		"bluethree": { main: "#0099c6", side: "#005e7a" },
+		"kamouni": { main: "#6B5E44", side: "#3B3426" },
+		"greyone": { main: "#474747", side: "#212121" },
+		"greytwo": { main: "#333", side: "#0D0D0D" }
 	},
-	nc = 10 /* Object.size(colors) */,
+	nc = 14 /* Object.size(colors) */,
 	lorem = ["Nam quis nulla.","Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet", "Etiam posuere quam ac quam.","In sem justo, commodo ut.","Morbi a metus. biglou sidi.","Praesent in mauris eu.", "Morbi leo mi, malou fati.","Maecenas ipsum velit","Aenean placerat. tirabichu."],
 	cache = []
 	lastTopOffset = 0;
 
-	$("article").each(function(i,elm){
+	$(".card").each(function(i,elm){
 
 		$this = $(this);
 
@@ -77,11 +35,11 @@ $(document).ready(function(){
 	}).bind("click", function(evt){
 		$this = $(this);
 		lastTopOffset = $this.offset().top;
-		var bg = $this.css("background-color");
-		var shadow = $this.css("text-shadow");
-		var color = shadow.replace(/ [0-9]*px/ig,"")
+		var dcolor = $this.data("color");
+        var bg = "#"+colors[dcolor].main;
+		var color = "#"+colors[dcolor].side;
 		$('body, body a').css({"background-color": bg})
-		$('#menu, #menu a').css({"background-color": bg, "text-shadow": shadow, "color": "white"})
+		$('#menu, #menu a').css({"background-color": bg, "text-shadow": "#"+color, "color": "white"})
 		$("#content")
 			.addClass("reading")
 			.css({
